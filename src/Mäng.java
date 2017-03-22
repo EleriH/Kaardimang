f54bc6b7f2c0f97db351205b81837724bdf22fb9
Eleri Hirv <eleri.hirv@gmail.com>
2017-03-22 21:39:02 +0200

    uuendused

--- src/Mäng.java
+++ src/Mäng.java
@@ -1,8 +1,33 @@
 import java.util.ArrayList;
+import java.util.Random;
 
 /**
  * Created by Eleri2 on 20.03.2017.
  */
 public class Mäng {
     private ArrayList<Kaart> kaardidPakis;
+    private ArrayList<Mängija> mängijad;
+
+    public Mäng(ArrayList<Kaart> kaardidPakis, ArrayList<Mängija> mängijad) {
+        this.kaardidPakis = kaardidPakis;
+        this.mängijad = mängijad;
+    }
+
+    public void mängi() {
+        for (Mängija k : mängijad) {
+            for (int i = 0; i < 8 ; i++) {
+                k.lisaKaart(kaardidPakis.get(0));
+                kaardidPakis.remove(0);
+            }
+        }
+
+        int järgmineAlustaja= new Random().nextInt(mängijad.size());
+        while (kaardidPakis.size()<mängijad.size()){
+            järgmineAlustaja= mänguring(järgmineAlustaja)
+        }
+    }
+
+    public int mänguring(int mängijaIndeks){
+
+    }
 }
--- src/Mängija.java
+++ src/Mängija.java
@@ -1,12 +1,23 @@
+import java.awt.*;
 import java.util.ArrayList;
 
 /**
  * Created by Eleri2 on 20.03.2017.
  */
-public class Mängija {
+public abstract class Mängija {
     private String nimi;
-    private ArrayList<Kaart>kaardidKäes;
+    private ArrayList<Kaart> kaardidKäes;
 
+    public Mängija(String nimi) {
+        this.nimi = nimi;
+        kaardidKäes = new ArrayList<>();
+    }
 
+    public void lisaKaart(Kaart kaart){
+        kaardidKäes.add(kaart);
+    }
 
+    public abstract Kaart käiKaart(List kaardidLaual);
+
+    
 }
